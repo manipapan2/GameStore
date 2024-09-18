99b1f71cfdf9c4b84f07f108f75ddca2ff15e12f
manipapan2 <manipapan2@gmail.com>
2024-09-18 23:30:06 +0330

    Updated

--- src/Components/UI/GameCards.tsx
+++ src/Components/UI/GameCards.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import {
