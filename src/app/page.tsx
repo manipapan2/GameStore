a366e8c88a3a8aa491edc338c395cf2e81eda32a
manipapan2 <manipapan2@gmail.com>
2024-09-19 12:37:11 +0330

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { Button } from "@/Components/UI/button";
 import GameCards from "@/Components/UI/GameCards";
+
+
 
 export default function Home() {
 	return (
 		<>
 			<GameCards />
 			<p className="bg-red-400 text-green-600">test</p>
+			<Button className="text-green-500">Test</Button>
 		</>
 	);
 }
